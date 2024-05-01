c84f4f3d2600dcccfd4229a23244879d241f5f1f
makeitworkAndris <andrisbarath5@gmail.com>
2024-05-01 19:05:35 +0200

    Skipping already downloaded files for each Directory + some documentation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,34 +16,37 @@ public class Main {
     static int endNumber = 347;
     static int attemptPagesPerChapter = 100;
     static String path = "C:\\Users\\Administrator\\Pictures\\Berserk_Chapters";
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         downloadChapters();
     }
-    public static void downloadChapters(){
-        try{
-            for(int i = startNumber; i <= endNumber; i++){
-                String currentChapter = "https://cdn.readberserk.com/file/mangap/1/20"+ i +"000/";
-                System.out.println(currentChapter);
 
-                String folder = "Chapter-"+i;
-                //Creates directory
-                File dir = new File(path+"\\"+folder);
-                if (!dir.exists()){
+    public static void downloadChapters() {
+        try {
+            for (int i = startNumber; i <= endNumber; i++) {
+                String currentChapter = "https://cdn.readberserk.com/file/mangap/1/20" + i + "000/";
+
+                System.out.println(currentChapter);
+                //Define path
+                String folder = "Chapter-" + i;
+                File dir = new File(path + "\\" + folder);
+                //Downloads the missing chapters
+                if (!dir.exists()) {
                     dir.mkdirs();
-                }
-                for(int page = 1; page <= attemptPagesPerChapter; page++){
-                    try{
-                        InputStream in = new URL(currentChapter+page+".jpg").openStream();
-                        Files.copy(in, Paths.get(path+"\\"+folder+"\\"+"Berserk-"+i+"-"+page+".jpg"));
-                    }
-                    catch (FileNotFoundException e){
-                        break;
-                    }
-                    catch (FileAlreadyExistsException ignored){
+                    for (int page = 1; page <= attemptPagesPerChapter; page++) {
+                        try {
+                            InputStream in = new URL(currentChapter + page + ".jpg").openStream();
+                            //SavePath + / + ChapterFolder + / + Berserk + currentChapterNumber + page
+                            Files.copy(in, Paths.get(path + "\\" + folder + "\\" + "Berserk-" + i + "-" + page + ".jpg"));
+
+                        } catch (FileNotFoundException e) {
+                            break;
+                        } catch (FileAlreadyExistsException ignored) {
+                        }
                     }
                 }
             }
-        }catch (IOException ignored){
+        } catch (IOException ignored) {
             System.out.println("Something failed");
         }
     }
